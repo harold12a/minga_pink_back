d00130d9d84936fa8108a1ced851485921f5db81
harold alzate <haroldalzate99@gmail.com>
2023-05-16 20:38:47 -0500

    manga-06

---
+++ models/Manga.js
@@ -0,0 +1,29 @@
+import { Schema,model,Types } from "mongoose";
+
+let collection = 'mangas'
+
+let schema = new Schema({
+  
+    author_id:{
+        type:Types.ObjectId,
+        ref: 'authors',
+        required:true
+    },
+    company_id:{
+        type:Types.ObjectId,
+        ref: 'users',
+        required:false
+    },
+    title:{type:String,required:true},
+    cover_photo:{type:String,required:true},
+    description:{type:String,required:true},
+    category_id:{
+        type:Types.ObjectId,
+        ref: 'users',
+        required:false
+    },
+},{timestamps:true})
+
+let Manga = model(collection,schema)
+
+export default Manga;
