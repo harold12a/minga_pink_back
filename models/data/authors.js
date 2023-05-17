546349c612c4df7a4d8568dbc03840a43280e992
harold alzate <haroldalzate99@gmail.com>
2023-05-16 19:53:42 -0500

    minga-04

---
+++ models/data/authors.js
@@ -0,0 +1,45 @@
+import 'dotenv/config.js'
+import '../../config/database.js'
+import Author from '../Author.js'
+
+let authors = [{
+    name: "alejandro",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '6462e146f79bf1be535b06a3'
+},{
+    name: "lucas",
+    last_name: "silva",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '6462e146f79bf1be535b06a4'
+},{
+    name: "jose",
+    last_name: "lopez",
+    city: "villa carlos paz",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '6462e146f79bf1be535b06a5'
+},{
+    name: "eric",
+    city: "resistencia",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '6462e146f79bf1be535b06a6'
+},{
+    name: "igna",
+    last_name: "borraz",
+    city: "rosario",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '6462e146f79bf1be535b06a7'
+}]
+
+Author.insertMany(authors)
