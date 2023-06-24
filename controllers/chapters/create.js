f6ee566c90bb109ecbe4134509a0c09dfa94d343
Jhoana Andrea Prada Gutierrez <125933648+andreaprada1993@users.noreply.github.com>
2023-06-23 20:18:46 -0500

    modificando control de chapter, create, creando company, modificando modelos router chapter (#11)

--- controllers/chapters/create.js
+++ controllers/chapters/create.js
@@ -3,13 +3,14 @@ import Chapter from '../../models/Chapter.js'
 
 export default async (req, res) => {
     try {
-        let data = req.body
+        let data = req.body 
         let one = await Chapter.create(data)
 
-        return res.status(201).json({
-            response: one,
-            message: 'created'
-        })
+            return res.status(201).json({
+                response: one,
+                message:'created'
+            })
+
     } catch (error) {
         return res.status(500).json({
             response: null,
--- models/Chapter.js
+++ models/Chapter.js
@@ -1,24 +1,19 @@
-import { Schema,model,Types } from "mongoose";
-
+import { Schema, model, Types } from "mongoose";
 
 let collection = 'chapters'
 
-
-
 let schema = new Schema({
-    manga_id:{
-        type:Types.ObjectId,
+    manga_id: {
+        type: Types.ObjectId,
         ref: 'mangas',
-        required:true
+        required: true
     },
-    title:{type:String,required:true},
-    cover_photo:{type:String,required:true},
+    title: { type: String, required: true },
+    cover_photo: { type: String, required: true },
+    pages: { type: Array, required: true },
+    order: { type: Number, required: true },
+}, { timestamps: true })
 
-    pages:{type:Array,required:true},
-
-    order:{type:Number,required:true},
-},{timestamps:true})
-
-let Chapter = model(collection,schema)
+let Chapter = model(collection, schema)
 
 export default Chapter;
