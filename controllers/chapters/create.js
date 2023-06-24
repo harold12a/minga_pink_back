c1acbb1caed9c82b1ecaa11529cffad6f3b7e46e
andreaprada1993 <andrea_prada_93@hotmail.com>
2023-06-23 20:11:57 -0500

    modificando control de chapter, create, creando company, modificando modelos router chapter

--- controllers/chapters/create.js
+++ controllers/chapters/create.js
@@ -4,24 +4,17 @@ import Chapter from '../../models/Chapter.js'
 export default async(req,res)=>{
     try {
         let data = req.body 
-        let one = Chapter.create(data)
-        if(one){
+        let one = await Chapter.create(data)
+
             return res.status(201).json({
                 response: one,
                 message:'created'
             })
-        }else{
-            return res.status(400).json({
-                response: null,
 
-                message:' Chapter NOT created'
-
-            })
-        }
     } catch (error) {
         return res.status(500).json({
             response: null,
-            message: 'not found!'
+            message: 'not created!'
         })
     }
 }
