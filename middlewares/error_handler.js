b00345a65dab7b08e72543a64367d6b9c321b967
Andrea Prada Gutierrez <125933648+andreaprada1993@users.noreply.github.com>
2023-06-28 14:32:19 -0500

    se agrega el middleware de add_cover_photo y se vincula en chaptaer y se arregla la clave en .env (#16)

--- middlewares/error_handler.js
+++
@@ -1,9 +0,0 @@
-const error_handler = (error,req,res,next) =>{
-    console.log(error)
-    return res.status(500).json({
-        response: null,
-        message: 'not created!'
-    })
-}
-
-export default error_handler
