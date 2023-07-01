35627600a794c8d0a1c1403e484d33788a69f29a
harold alzate <haroldalzate99@gmail.com>
2023-07-01 17:11:01 -0500

    se modifica controllers auts read - singin companies create - read se agrega middlewares exists_orders is_active is_property_of se modifica middlewares passport router auth

--- middlewares/createHash.js
+++ middlewares/createHash.js
@@ -1,11 +1,7 @@
 import bcryptjs from 'bcryptjs';
 export default (req,res,next)=>{
     let password = req.body.password
-    let hashPassword = bcryptjs.hashSync(
-        password, 
-        10              // nivel de seguridad confiable 
-
-    )
+    let hashPassword = bcryptjs.hashSync(password, 10 )             // nivel de seguridad confiable 
     req.body.password = hashPassword
     return next()
 
