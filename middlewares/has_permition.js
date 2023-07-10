8513d52cab248332a008b64fe4bbee209e5a052c
Andrea Prada Gutierrez <125933648+andreaprada1993@users.noreply.github.com>
2023-07-10 18:16:40 -0500

    M04 middlewares2 (#26)
    * modificaciones en la ruta chapter
    
    * mensajes creatos de chapter

--- middlewares/has_permition.js
+++ middlewares/has_permition.js
@@ -1,7 +1,6 @@
 import Author from "../models/Author.js"
 import Company from "../models/Company.js"
 
-
 export default async (req,res,next) =>{
     
     if(req.user.role ===1 || req.user.role ===2){
@@ -20,6 +19,8 @@ export default async (req,res,next) =>{
     return res.status(404).json({
         success: true,
         response:null,
+
         messages: ['not allow']
+
     })
 }
